4ba54f3a2497de58906d3311cd0e7fc017a09540
ckwogus37 <ckwogus37@gmail.com>
2024-12-21 18:31:24 +0900

    chore : 프로젝트 폴더 최초 커밋

---
+++ src/main/java/JHToyProject/Manote/MaNoteApplication.java
@@ -0,0 +1,12 @@
+package JHToyProject.Manote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaNoteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MaNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JHToyProject/Manote/MaNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package JHToyProject.Manote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
